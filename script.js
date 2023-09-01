385951b40367b934140fcb19f8d47d1f9f7fc042
MOHAMMAD NOUSHAD <116102583+noushadkas@users.noreply.github.com>
2023-09-01 17:51:54 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
 //your JS code here. If required.
+// Function to get the current date and time in the desired format
+function getCurrentDateTime() {
+  const now = new Date();
+  const date = now.toLocaleDateString();
+  const time = now.toLocaleTimeString();
+  return `${date}, ${time}`;
+}
+
+// Function to update the timer every second
+function updateTimer() {
+  const timerElement = document.getElementById('timer');
+  timerElement.textContent = getCurrentDateTime();
+}
+
+// Call the updateTimer function every second
+setInterval(updateTimer, 1000);
